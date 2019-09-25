f1e17ffa7b6d82249c024d0783f559ca93bcf565
Matthew C <m23.cullen@gmail.com>
2019-09-25 01:40:22 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,8 +2,9 @@ class Post
   attr_accessor :title, :author
   @@all = []
     
-    def initialize(title)
+    def initialize(title,author = nil)
       @title = title
+      @author = author
       @@all << self
     end
     

d774769ead669062e0eb8956342fc571d4d6f999
Matthew C <m23.cullen@gmail.com>
2019-09-25 01:36:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author 
   attr_accessor :name
-  @@posts = 1
+  @@post_count = 1
   
   def initialize(name)
     @name = name 
@@ -12,12 +12,13 @@ class Author
   
   def add_post(post)
     post.author = self
+    @@post_count += 1
   end 
   
   def add_post_by_title(title) 
     new_post = Post.new(title)
     new_post.author = self 
-    
+    @@post_count += 1
   end 
   
   def self.post_count

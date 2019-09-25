2add24e90992d5e3c9d0a32d5c1f0142072e6f28
Matthew C <m23.cullen@gmail.com>
2019-09-25 00:56:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist 
   attr_accessor :name, :songs
   @@all = []
-  @@song_count
+  @@song_count = 0 
   
   def initialize(name)
     @name = name 
@@ -28,12 +28,13 @@ class Artist
   
   def add_song(song)
     song.artist = self 
+    @@song_count += 1
   end 
   
   def add_song_by_name(song_name)
     new_song = Song.new(song_name)
     add_song(new_song)
-    
+    @@song_count += 1
   end 
   
   def self.song_count
